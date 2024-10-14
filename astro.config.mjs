a5b0d341fd4121b364b000d69004a2cc67159aea
Pedro-CV <pedro@ssventures.com>
2024-10-14 10:28:18 -0400

    add actions to deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
-import { defineConfig } from 'astro/config'
-import tailwind from "@astrojs/tailwind"
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 
-import robotsTxt from "astro-robots-txt"
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
-  site: 'https://pedro-enrique.github.io'
-})
+  site: "https://pedro-cv.github.io",
+});
